eeacb0b280b881c90b7ed5be10197adfaf53c4f2
FIRST_NAME LAST_NAME <abdiafrah1997@outlook.com>
2022-03-17 20:33:33 +0000

    inital commit

---
+++ src/components/Film.js
@@ -0,0 +1,14 @@
+const FIlm = ({film}) => {
+
+
+return (
+    <>
+    
+    
+    
+    </>
+)
+
+
+
+}
---
+++ src/containers/FilmContainer.js
@@ -0,0 +1,73 @@
+import { useState } from "react";
+
+
+const FilmContainer =() => {
+
+
+    const [films, setFilms] = useState (
+    [
+        {
+            "title": "Alien",
+            "duration": 117,
+            "rating": "18",
+            "cast": [
+                {
+                    "name": "Sigourney Weaver"
+                }
+            ],
+            "director": {
+                "name": "Ridley Scott"
+            }
+        },
+        {
+            "title": "The Imitation Game",
+            "duration": 114,
+            "rating": "12A",
+            "cast": [
+                {
+                    "name": "Benedict Cumberbatch"
+                }
+            ],
+            "director": {
+                "name": "Morten Tyldum"
+            }
+        },
+        {
+            "title": "Iron Man",
+            "duration": 126,
+            "rating": "12A",
+            "cast": [
+                {
+                    "name": "Robert Downey Jr"
+                },
+                {
+                    "name": "Gwyneth Paltrow"
+                }
+            ],
+            "director": {
+                "name": "Jon Favreau"
+            }
+        },
+        {
+            "title": "The Martian",
+            "duration": 144,
+            "rating": "12A",
+            "cast": [
+                {
+                    "name": "Matt Damon"
+                },
+                {
+                    "name": "Sean Bean"
+                }
+            ],
+            "director": {
+                "name": "Ridley Scott"
+            }
+        }
+    ]
+
+    )
+}
+
+export default FilmContainer;
+
